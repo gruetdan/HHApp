60de22420d6d2d28cd10abb031d91b51baf38078
allenano <allenano@zhaw.students.ch>
2025-05-29 17:07:39 +0200

    labels ergänzt

--- src/main/java/com/zhaw/hhapp/Main.java
+++ src/main/java/com/zhaw/hhapp/Main.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 
 import static javafx.application.Application.launch;
 
-//Hallo, dies ist eine änderung
-
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
