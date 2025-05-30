e6af4dab6f8123ecd665b77c7262b2bca5ccb53e
allenano <allenano@zhaw.students.ch>
2025-05-30 20:30:25 +0200

    added comments

--- src/main/java/com/zhaw/hhapp/manager/ExpenseManager.java
+++ src/main/java/com/zhaw/hhapp/manager/ExpenseManager.java
@@ -4,16 +4,25 @@ import com.zhaw.hhapp.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+/**
+ * Manager class responsible for displaying the detail view of a single ExpenseList.
+ * <p>
+ * This class creates and shows a new JavaFX window (stage) for managing a specific ExpenseList.
+ * It loads the corresponding FXML view and sets the window title to the given list name.
+ * </p>
+ */
 public class ExpenseManager {
 
-    FXMLLoader fxmlLoader;
+    /** The FXMLLoader used to load the ExpenseView FXML. */
+    //Note: can be moved to constructor as a local variable if not used otherwise
+     FXMLLoader fxmlLoader;
+
     /**
-     * Sets up the second level scene (Scene for particular ExpenseList)
+     * Opens a new window (stage) to manage the specified ExpenseList.
      *
-     * @param expenseListId ist key/name of particular ExpenseList
+     * @param expenseListId The key/name of the particular ExpenseList to be managed.
      */
     public ExpenseManager(String expenseListId) {
 
@@ -23,7 +32,7 @@ public class ExpenseManager {
             scene = new Scene(fxmlLoader.load());
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Fehler beim Laden der FXML-Datei!");
+            System.out.println("Error loading the fxml file!");
         }
         Stage stage = new Stage();
         stage.setTitle(expenseListId);
